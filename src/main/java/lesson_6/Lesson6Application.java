14db4795d56a74da7e47f53c807d0a7436853b39
Mirmukhsin <mirmuhsin9779@gmail.com>
2024-05-13 22:32:13 +0500

    second commit

--- src/main/java/lesson_6/Lesson6Application.java
+++ src/main/java/lesson_6/Lesson6Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 
@@ -13,6 +14,7 @@ import java.util.Collections;
 import java.util.List;
 
 @SpringBootApplication
+@EnableCaching
 public class Lesson6Application {
 
     public static void main(String[] args) {
